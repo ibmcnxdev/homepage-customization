97ee12602abc645b3b197b0649919c2dee0a7fc8
enunezcv3 <37451579+enunezcv3@users.noreply.github.com>
2018-03-26 08:59:23 -0600

    Update helloWorld.user.js

--- helloWorld.user.js
+++ helloWorld.user.js
@@ -38,7 +38,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Hello World! ";
+   			dojo.query("span.shareSome-title")[0].textContent="Hello CV3 Collaborator! ";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
