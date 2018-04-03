8711a18fd28897bc8de0dcd769f018297b3b75a9
enunezcv3 <37451579+enunezcv3@users.noreply.github.com>
2018-04-03 13:48:32 -0600

    Update css_profile_enm.js

--- css_profile/css_profile_enm.js
+++ css_profile/css_profile_enm.js
@@ -2,7 +2,7 @@ if (typeof (dojo) != "undefined") {
     require(["dojo/domReady!"], function () {
         try {
             dojo.place(
-               "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/profiles/css_profile_enm.css?repoName=homepage-customization\"></link>",
+               "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/css_profile/css_profile_enm.css?repoName=homepage-customization\"></link>",
 				dojo.doc.head,
 				"last"
             );
